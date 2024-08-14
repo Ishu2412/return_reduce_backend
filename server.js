85960b327b427b85385093b7f6163d401c7aec4f
Shrayash Shukla <ishrayash@gmail.com>
2024-08-14 11:55:53 +0530

    14-8-2024 3

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
+app.use(authenticationRouter);
 app.use(sizeRouter);
 app.use(genuineRouter);
 app.use(get404);
